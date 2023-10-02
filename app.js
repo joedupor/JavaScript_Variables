21ce14c13914408d7627b5398e9cced8915f295d
joedupor <joedupor@gmail.com>
2023-10-01 21:17:44 -0500

    finished exercise

--- app.js
+++ app.js
@@ -3,8 +3,55 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 & 2
+const firstName = `Bruce`;
+let lastName = `Wayne`;
+var age = 42;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(`First Name: ${firstName} -- Last Name: ${lastName} -- Age: ${age}`);
 
-// Exercise 2
+// const firstName = `Lil`;  this will not work, CANNOT reassign a const
+lastName = `Springsteen`;
+age = 62;
+console.log(`First Name: ${firstName} -- Last Name: ${lastName} -- Age: ${age}`);
+
 
 // Exercise 3
+let language = `JavaScript`;
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`${language} was first created in 
+the year ${createdYear}. This is in fact, ${isCaseSensitive}!`);
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = `M`;
+console.log(`${price} ${isOnSale} ${salePercentage} ${stock} ${inStock} ${selectedSize}`);
+
+//Exercise 5
+let title = `Name of the Wind`;
+let author = `Patrick Rothfuss`;
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(`${title} ${author} ${pageCount} ${bookmark} ${hasRead}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
